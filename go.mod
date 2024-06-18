17b2efca91f83a57f594f51aaafb0d2d18991588
Digkill <digkill@mail.ru>
2024-06-19 03:33:20 +0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/digkill/tgBotGo
+
+go 1.21.1
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
+	fmt.Printf("TELEGRAM BOT TOKEN: %s", os.Getenv("TELEGRAM_BOT_TOKEN"))
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.IsCommand() {
+			handleCommand(bot, update.Message)
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello! Choose an action:")
+		msg.ReplyMarkup = getMainKeyboard()
+		bot.Send(msg)
+	}
+
+	fmt.Println("Bot ready!")
+}
+
+func handleCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	switch message.Command() {
+	case "start":
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Добро пожаловать в бот.")
+		msg.ReplyMarkup = getMainKeyboard()
+		bot.Send(msg)
+	default:
+		msg := tgbotapi.NewMessage(message.Chat.ID, "I don't know that command.")
+		bot.Send(msg)
+	}
+}
+
+func getMainKeyboard() tgbotapi.ReplyKeyboardMarkup {
+	button1 := tgbotapi.NewKeyboardButton("Выгрузить")
+	row1 := tgbotapi.NewKeyboardButtonRow(button1)
+
+	keyboard := tgbotapi.NewReplyKeyboard(row1)
+	return keyboard
+}
